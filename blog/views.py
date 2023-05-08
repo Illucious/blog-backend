a30edd599f9c63c3b4c7d6f0b2a4ac880e1b7069
Illucious <visheshgarg570@gmail.com>
2023-05-08 16:48:59 +0530

    added initial views and url logic

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'blog/index.html')
+
+
+def posts(request):
+    return None
+
+
+def post_details(request):
+    pass
